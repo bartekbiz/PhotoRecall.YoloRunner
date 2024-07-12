a3b9e6efdc63da3ff05fc21fcdff142db3acda50
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-07-12 12:09:27 +0200

    add download method and documentation to FileUtils and YoloRunner

--- yolo_runner_base/utils/file_utils.py
+++ yolo_runner_base/utils/file_utils.py
@@ -1,7 +1,31 @@
+import os
 import shutil
+import requests
 
 
 class FileUtils:
-    @staticmethod
-    def clear_data_dir():
-        shutil.rmtree("data/")
+    data_dir_path = "data/"
+
+    def download(self, url: str) -> str:
+        """
+        Downloads file given in url.
+        :param url:
+        :return: Returns path to saved file.
+        """
+        get_response = requests.get(url, stream=True)
+
+        save_path = f"{self.data_dir_path}{url.split("/")[-1]}"
+
+        with open(save_path, 'wb') as f:
+            for chunk in get_response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+        return save_path
+
+    def clear_data_dir(self):
+        """
+        Clears content of data directory.
+        """
+        shutil.rmtree(self.data_dir_path)
+        os.mkdir(self.data_dir_path)
--- yolo_runner_base/yolo_runner/yolo_runner.py
+++ yolo_runner_base/yolo_runner/yolo_runner.py
@@ -1,13 +1,20 @@
 import logging
 
 from abc import ABC, abstractmethod
+from yolo_runner_base.utils.file_utils import FileUtils
 
 
 class YoloRunner(ABC):
     def __init__(self):
         self.logger = logging.getLogger()
         self.model = None
+        self.file_utils = FileUtils()
 
     @abstractmethod
-    def predict(self, photo_url: str):
+    def predict_from_url(self, photo_url: str):
+        """
+        Runs prediction for photo given as url.
+        :param photo_url:
+        :return: Returns prediction results as json.
+        """
         pass
