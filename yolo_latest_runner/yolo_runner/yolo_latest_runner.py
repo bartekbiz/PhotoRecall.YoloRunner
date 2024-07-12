84056d1d45831ccd0359c2cec3646eb09544bb53
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-07-12 12:10:04 +0200

    change predict to predict_from_url, use FileUtils in prediction method

--- yolo_latest_runner/yolo_runner/yolo_latest_runner.py
+++ yolo_latest_runner/yolo_runner/yolo_latest_runner.py
@@ -1,6 +1,7 @@
 import json
 
 from yolo_runner_base.yolo_runner.yolo_runner import YoloRunner
+
 from ultralytics import YOLO
 
 
@@ -9,9 +10,12 @@ class YoloLatestRunner(YoloRunner):
         super().__init__()
         self.model = YOLO(f"models/{model_name}")
 
-    def predict(self, photo_url: str):
-        prediction = self.model(photo_url)
+    def predict_from_url(self, photo_url: str):
+        photo_path = self.file_utils.download(photo_url)
+
+        prediction = self.model(photo_path)
+
+        self.file_utils.clear_data_dir()
 
         prediction_json = prediction[0].tojson()
-
         return json.loads(prediction_json)
