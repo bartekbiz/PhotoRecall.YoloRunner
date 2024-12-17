0ba7033ac6616ed9d82fe366423dcbec21ad1dd6
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-12-17 18:05:00 +0100

    Try using garbage collector to free up server's memory

--- yolo_ultralytics_runner/main.py
+++ yolo_ultralytics_runner/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import gc
 
 from yolo_runner.yolo_ultralytics_runner import YoloUltralyticsRunner
 from yolo_runner_base.utils.other_utils import OtherUtils
@@ -9,4 +10,6 @@ app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True, "tryItOutEnabled":
 @app.get("/predict")
 def predict(photo_url: str, model_name: str = "yolov10n.pt"):
     yolo_runner = YoloUltralyticsRunner(model_name)
-    return OtherUtils.measure_time(yolo_runner.predict, photo_url)
+    result = OtherUtils.measure_time(yolo_runner.predict, photo_url)
+    gc.collect()
+    return result
