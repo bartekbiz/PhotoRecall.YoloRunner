bc6cd4655d51af0608be10376b2235e9e242a5b8
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-12-17 18:37:19 +0100

    Try using garbage collector differently

--- yolo_ultralytics_runner/main.py
+++ yolo_ultralytics_runner/main.py
@@ -9,7 +9,6 @@ app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True, "tryItOutEnabled":
 
 @app.get("/predict")
 def predict(photo_url: str, model_name: str = "yolov10n.pt"):
-    yolo_runner = YoloUltralyticsRunner(model_name)
-    result = OtherUtils.measure_time(yolo_runner.predict, photo_url)
     gc.collect()
-    return result
+    yolo_runner = YoloUltralyticsRunner(model_name)
+    return OtherUtils.measure_time(yolo_runner.predict, photo_url)
